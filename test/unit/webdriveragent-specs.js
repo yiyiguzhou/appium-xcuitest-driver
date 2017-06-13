abeb8c14922fa657d15d84bd4f4127e3baf8f0e1
Isaac Murchie <isaac@saucelabs.com>
2017-06-13 08:21:56 -0400

    Make switch between launchers possible

--- test/unit/webdriveragent-specs.js
+++ test/unit/webdriveragent-specs.js
@@ -1,55 +1,27 @@
 import { WebDriverAgent } from '../../lib/wda/webdriveragent';
-import { BOOTSTRAP_PATH } from '../../lib/wda/xcodebuild';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import path from 'path';
-import _ from 'lodash';
 
 
 chai.should();
 chai.use(chaiAsPromised);
 
 const fakeConstructorArgs = {
-  device: 'some sim',
+  device: {
+    udid: 1,
+  },
   platformVersion: '9',
   host: 'me',
   port: '5000',
-  realDevice: false
+  realDevice: false,
 };
-
-const defaultAgentPath = path.resolve(BOOTSTRAP_PATH, 'WebDriverAgent.xcodeproj');
-const customBootstrapPath = '/path/to/wda';
-const customAgentPath = '/path/to/some/agent/WebDriverAgent.xcodeproj';
-
-describe('Constructor', () => {
-  it('should have a default wda agent if not specified', () => {
-    let agent = new WebDriverAgent({}, fakeConstructorArgs, false);
-    agent.testrunner.bootstrapPath.should.eql(BOOTSTRAP_PATH);
-    agent.testrunner.agentPath.should.eql(defaultAgentPath);
-  });
-  it('should have custom wda bootstrap and default agent if only bootstrap specified', () => {
-    let agent = new WebDriverAgent({}, _.defaults({
-      bootstrapPath: customBootstrapPath,
-    }, fakeConstructorArgs), false);
-    agent.testrunner.bootstrapPath.should.eql(customBootstrapPath);
-    agent.testrunner.agentPath.should.eql(path.resolve(customBootstrapPath, 'WebDriverAgent.xcodeproj'));
-  });
-  it('should have custom wda bootstrap and agent if both specified', () => {
-    let agent = new WebDriverAgent({}, _.defaults({
-      bootstrapPath: customBootstrapPath,
-      agentPath: customAgentPath,
-    }, fakeConstructorArgs), false);
-    agent.testrunner.bootstrapPath.should.eql(customBootstrapPath);
-    agent.testrunner.agentPath.should.eql(customAgentPath);
-  });
-});
 
 describe('launch', () => {
   it('should use webDriverAgentUrl override', async () => {
     let override = "http://mockUrl:8100";
     let args = Object.assign({}, fakeConstructorArgs);
     args.webDriverAgentUrl = override;
-    let agent = new WebDriverAgent({}, args);
+    let agent = new WebDriverAgent({}, 'xcodebuild', args);
 
     (await agent.launch("sessionId")).should.be.equal(override);
   });
